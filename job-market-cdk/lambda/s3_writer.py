99fb56c0a1ff957f4687ba63fa9c6813af10637e
akhoshrozeh <akhoshrozeh@gmail.com>
2024-12-14 20:33:19 -0800

    check if in s3 already

--- job-market-cdk/lambda/s3_writer.py
+++ job-market-cdk/lambda/s3_writer.py
@@ -1,6 +1,10 @@
 import json
 import boto3
 import os
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
     s3 = boto3.client('s3')
@@ -15,46 +19,60 @@ def handler(event, context):
             
             # Debugging: Print the record
             jsonified = json.dumps(job_record, indent=4)
-            # print("pretty", jsonified["job"])
+
             
             # Extract the job ID for S3 key
             job_id = record.get('job', {}).get('id', 'unknown_id')
             site = record.get('job', {}).get('site', 'unknown_site')
+            s3_key = f"data/{site}/{job_id}.json"
+
+            try:
+                s3.head_object(Bucket=os.environ['BUCKET_NAME'], Key=s3_key)
+                logger.info(f"Object {s3_key} already exists in S3.")
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps(f'Object {s3_key} already exists in S3')
+                }
+            except Exception as e:
+                logger.info(f"Object {s3_key} does not exist in S3. Writing...")
             
-            # Write record to S3
-            res = s3.put_object(
-                Bucket=os.environ['BUCKET_NAME'],
-                Key=f"data/{site}/{job_id}.json",
-                Body=jsonified  # Convert to JSON string
-            )
-
-            print("S3 Response:", res)
-
-            # make sure
-            if res.get('ResponseMetadata', {}).get('HTTPStatusCode') == 200:
-                event_bus.put_events(
-                    Entries=[
-                        {
-                            'Source': 's3.writer',
-                            'DetailType': 'S3WriteCompleteEvent',
-                            'Detail': jsonified
-                        }
-                    ]
+                # Write record to S3
+                res = s3.put_object(
+                    Bucket=os.environ['BUCKET_NAME'],
+                    Key=f"data/{site}/{job_id}.json",
+                    Body=jsonified  # Convert to JSON string
                 )
 
-            return {
-                'statusCode': 200,
-                'body': json.dumps('Data written to S3')
-            }
+
+
+                # make sure
+                if res.get('ResponseMetadata', {}).get('HTTPStatusCode') == 200:
+                    event_bus.put_events(
+                        Entries=[
+                            {
+                                'Source': 's3.writer',
+                                'DetailType': 'S3WriteCompleteEvent',
+                                'EventBusName': os.environ['EVENT_BUS_NAME'],
+                                'Detail': jsonified
+                            }
+                        ]
+                    )
+
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps('Data written to S3')
+                }
         else:
             # Handle case where "detail" is missing
             return {
                 'statusCode': 400,
                 'body': json.dumps('Missing "detail" in event payload')
             }
+
+
     except Exception as e:
         # Catch and log errors
-        print("Error:", str(e))
+        logger.error("Error:", str(e))
         return {
             'statusCode': 500,
             'body': json.dumps({'error': str(e)})
