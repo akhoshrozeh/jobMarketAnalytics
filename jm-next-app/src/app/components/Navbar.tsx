0b9d89afce2e8559f368ba42f814042b6082e9c5
akhoshrozeh <akhoshrozeh@gmail.com>
2024-12-31 16:52:30 -0800

    frontend ssr mongo test

--- jm-next-app/src/app/components/Navbar.tsx
+++ jm-next-app/src/app/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Navbar() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
@@ -19,27 +21,27 @@ export default function Navbar() {
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                <a href="/" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
+                <Link href="/" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
                   Home
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                 >
                   Team
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                 >
                   Pricing
-                </a>
-                <a
+                </Link>
+                <Link
                   href="#"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                 >
                   [empty]
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -60,11 +62,6 @@ export default function Navbar() {
                   <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <span className="absolute -inset-1.5" />
                     <span className="sr-only">Open user menu</span>
-                    <img
-                      alt=""
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      className="size-8 rounded-full"
-                    />
                   </MenuButton>
                 </div>
                 <MenuItems
@@ -72,28 +69,28 @@ export default function Navbar() {
                   className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                 >
                   <MenuItem>
-                    <a
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                     >
                       Your Profile
-                    </a>
+                    </Link>
                   </MenuItem>
                   <MenuItem>
-                    <a
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                     >
                       Settings
-                    </a>
+                    </Link>
                   </MenuItem>
                   <MenuItem>
-                    <a
+                    <Link
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                     >
                       Sign out
-                    </a>
+                    </Link>
                   </MenuItem>
                 </MenuItems>
               </Menu>
@@ -146,7 +143,7 @@ export default function Navbar() {
         <div className="border-t border-gray-700 pb-3 pt-4">
           <div className="flex items-center px-5">
             <div className="shrink-0">
-              <img
+              <Image
                 alt=""
                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 className="size-10 rounded-full"
--- jm-next-app/src/app/page.tsx
+++ jm-next-app/src/app/page.tsx
@@ -1,7 +1,60 @@
-export default function Home() {
+import { Resource } from "sst";
+const APIEndpoint = Resource.APIEndpoint.value;
+
+async function test() {
+  try {
+    const response = await fetch(`${APIEndpoint}/hello`, 
+      { 
+        headers: {
+          'Accept': 'application/json',
+        },
+        cache: 'no-store' 
+      }
+    );    
+    console.log(response)
+    if (!response.ok) {
+      throw new Error("Fail to fetch data.")
+    }
+    return response.json()
+
+  } catch (error) {
+    console.error("Error caught:", error)
+    return {message: "error fetching data."}
+  }
+}
+
+async function getAvgOcc() {
+  const mongoURI = Resource.MongoReadURI.value;
+  try {
+    const response = await fetch(`${APIEndpoint}/avg-occ`, {
+      headers: {
+        'Accept': 'application/json',
+      }
+    })
+
+    console.log(response)
+    if (!response.ok) {
+      throw new Error("Failed to fetch from mongo")
+    }
+
+    return response.json()
+
+  } catch (error) {
+    console.error("err caught:", error)
+  }
+}
+
+export default async function Home() {
+  const data = await test();
+  const mongodata = await getAvgOcc();
   return (
     <div>
-      <h1>Job Market</h1>
-    </div>
+      <h1>Lambda Function Test</h1>
+      <p>Message from lambda: {data.message}</p>
+      <h1>Test Read From Mongo</h1>
+      <p>Values: {mongodata && mongodata.map((item:any, index: number) => (
+                <span key={index}>{JSON.stringify(item)}{index < mongodata.length - 1 ? ', ' : ''}</span>
+      ))}</p>
+     </div>
   );
 }
