07b479ccf2f71364aa2663879befe3e3c9326069
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-01 14:52:50 -0800

    navbar links updated

--- jm-next-app/src/app/components/Navbar.tsx
+++ jm-next-app/src/app/components/Navbar.tsx
@@ -1,47 +1,43 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
-import Link from 'next/link'
-
-export default function Navbar() {
+export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-900">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
             <div className="shrink-0">
-              <Image
+              <img
                 alt="Your Company"
                 src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
                 className="h-8 w-auto"
-                width={32}  
-                height={32}
               />
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                <Link href="/" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
+                <a href="/" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
                   Home
-                </Link>
-                <Link
-                  href="#"
+                </a>
+                <a
+                  href="/metrics"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                 >
-                  Team
-                </Link>
-                <Link
-                  href="#"
+                  Metrics
+                </a>
+                <a
+                  href="/jobs"
+                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                >
+                  Jobs
+                </a>
+                <a
+                  href="Pricing"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                 >
                   Pricing
-                </Link>
-                <Link
-                  href="#"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  [empty]
-                </Link>
+                </a>
               </div>
             </div>
           </div>
@@ -62,6 +58,13 @@ export default function Navbar() {
                   <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <span className="absolute -inset-1.5" />
                     <span className="sr-only">Open user menu</span>
+                    {/* <Image
+                      alt=""
+                      src=""
+                      className="size-8 rounded-full"
+                      height={32}
+                      width={32}
+                    /> */}
                   </MenuButton>
                 </div>
                 <MenuItems
@@ -69,28 +72,28 @@ export default function Navbar() {
                   className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                 >
                   <MenuItem>
-                    <Link
+                    <a
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                     >
                       Your Profile
-                    </Link>
+                    </a>
                   </MenuItem>
                   <MenuItem>
-                    <Link
+                    <a
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                     >
                       Settings
-                    </Link>
+                    </a>
                   </MenuItem>
                   <MenuItem>
-                    <Link
+                    <a
                       href="#"
                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                     >
                       Sign out
-                    </Link>
+                    </a>
                   </MenuItem>
                 </MenuItems>
               </Menu>
@@ -116,7 +119,7 @@ export default function Navbar() {
             href="#"
             className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
           >
-            Comp/
+            Dashboard
           </DisclosureButton>
           <DisclosureButton
             as="a"
@@ -130,7 +133,7 @@ export default function Navbar() {
             href="#"
             className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
           >
-            Pricing
+            Projects
           </DisclosureButton>
           <DisclosureButton
             as="a"
@@ -143,7 +146,7 @@ export default function Navbar() {
         <div className="border-t border-gray-700 pb-3 pt-4">
           <div className="flex items-center px-5">
             <div className="shrink-0">
-              <Image
+              <img
                 alt=""
                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 className="size-10 rounded-full"
