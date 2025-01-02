c13fb861d1799308b11b11f76a04d786bb8b1440
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-02 11:16:29 -0800

    update mobile nav links

--- jm-next-app/src/app/components/Navbar.tsx
+++ jm-next-app/src/app/components/Navbar.tsx
@@ -74,28 +74,28 @@ export default function Example() {
                   className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                 >
                   <MenuItem>
-                    <a
-                      href="#"
+                    <Link
+                      href="/"
                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                     >
-                      Your Profile
-                    </a>
+                      Home
+                    </Link>
                   </MenuItem>
                   <MenuItem>
-                    <a
-                      href="#"
+                    <Link
+                      href="/metrics"
                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                     >
-                      Settings
-                    </a>
+                      Metrics
+                    </Link>
                   </MenuItem>
                   <MenuItem>
-                    <a
-                      href="#"
+                    <Link
+                      href="/jobs"
                       className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                     >
-                      Sign out
-                    </a>
+                      Jobs
+                    </Link>
                   </MenuItem>
                 </MenuItems>
               </Menu>
