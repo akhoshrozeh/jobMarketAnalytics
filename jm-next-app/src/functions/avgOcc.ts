e69cb3ff33d093207dbc603a051d969f93f33d1f
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-02 11:03:14 -0800

    separate mongodb cxn

--- jm-next-app/src/functions/avgOcc.ts
+++ jm-next-app/src/functions/avgOcc.ts
@@ -1,26 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { Resource } from "sst"
 import * as mongodb from "mongodb";
-
-const MongoClient = mongodb.MongoClient;
-const mongoURI = Resource.MongoReadURI.value;
-const db = Resource.JMDatabase.value;
-
-let cachedDb: mongodb.Db | null = null;
-
-async function connectToDatabase() {
-  if (cachedDb) {
-    return cachedDb;
-  }
-
-  // Connect to our MongoDB database hosted on MongoDB Atlas
-  const client = await MongoClient.connect(mongoURI);
-
-  // Specify which database we want to use
-  cachedDb = await client.db(db);
-
-  return cachedDb;
-}
+import { connectToDatabase } from "@/lib/mongoClient";
 
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult>  => {
