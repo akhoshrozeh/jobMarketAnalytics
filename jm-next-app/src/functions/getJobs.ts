e7f05e51c48e682ef69a1399e4262e04f44319b0
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-02 19:18:17 -0800

    job page & lambda

---
+++ jm-next-app/src/functions/getJobs.ts
@@ -0,0 +1,30 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { connectToDatabase } from "@/lib/mongoClient";
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult>  => {
+    console.log("GetJobs handler started", {
+        httpMethod: event.httpMethod,
+        path: event.path,
+        headers: event.headers
+    });
+
+    const db = await connectToDatabase();
+    console.log("Database connected successfully");
+
+
+    try {
+        // Temporarily only getting jobs from indeed
+        const result = await db.collection('JobPostings').find({"site": "indeed"}).sort({date_posted: -1}).limit(100).toArray();
+        console.log("RESULTS: ", result)
+        return {
+            statusCode: 200,
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(result)
+        };
+    } catch (error) {
+        console.error(error)
+        return { statusCode: 500, body: JSON.stringify({ error: 'Internal server error' }) };
+    }
+}
---
+++ jm-next-app/src/types/JobPostType.ts
@@ -0,0 +1,33 @@
+export type JobPost = {
+    currency: string | null;
+    company_logo: string | null;
+    company_addresses: string | null;
+    min_amount: number | null;
+    location: string | null;
+    job_url_direct: string | null;
+    description: string | null;
+    id: string | null;
+    listing_type: string;
+    _id: string;
+    date_posted: string;
+    emails: string;
+    company_url_direct: string;
+    extracted_keywords: string[];
+    max_amount: number | null;
+    company_url: string | null;
+    company_industry: string | null;
+    company_description: string | null;
+    interval: string | null;
+    is_remote: boolean | null;
+    title: string;
+    company_num_employees: string | null;
+    salary_source: string | null;
+    job_url: string;
+    site: string;
+    job_function: string | null;
+    company: string | null;
+    job_level: string | null;
+    job_type: string | null;
+    company_revenue: string | null;
+}
+
