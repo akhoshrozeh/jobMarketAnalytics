c99ff5ab7935ae77a4283584da1b5dba611e9bd4
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-03 22:51:09 -0800

    add scraping params into lambda

--- job-market-cdk/lambda/jobspy_scrapers.py
+++ job-market-cdk/lambda/jobspy_scrapers.py
@@ -8,63 +8,131 @@ import time
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+def get_scrape_params():
+    return [
+        {
+                "site_name": ["indeed"],
+                "search_term": "software engineer",
+                "location": "San Francisco, CA",
+                "results_wanted": 100,
+                "hours_old": 72,
+                "country_indeed": "USA"
+        },
+        {
+                "site_name": ["indeed"],
+                "search_term": "software engineer",
+                "location": "Los Angeles, CA",
+                "results_wanted": 100,
+                "hours_old": 72,
+                "country_indeed": "USA"
+        },
+        {
+                "site_name": ["indeed"],
+                "search_term": "software engineer",
+                "location": "New York City, NY",
+                "results_wanted": 100,
+                "hours_old": 72,
+                "country_indeed": "USA"
+        },
+        {
+                "site_name": ["indeed"],
+                "search_term": "software engineer",
+                "location": "Seattle, WA",
+                "results_wanted": 100,
+                "hours_old": 72,
+                "country_indeed": "USA"
+        },
+        {
+                "site_name": ["indeed"],
+                "search_term": "software engineer",
+                "location": "Boston, MA",
+                "results_wanted": 100,
+                "hours_old": 72,
+                "country_indeed": "USA"
+        },
+        {
+                "site_name": ["indeed"],
+                "search_term": "software engineer",
+                "location": "San Diego, CA",
+                "results_wanted": 100,
+                "hours_old": 72,
+                "country_indeed": "USA"
+
+        },
+        {
+                "site_name": ["indeed"],
+                "search_term": "software engineer",
+                "location": "Chicago, IL",
+                "results_wanted": 100,
+                "hours_old": 72,
+                "country_indeed": "USA"
+        }
+    ]
+    
 def handler(event, context):
     eventbridge = boto3.client('events')
-    
-    required_params = ['site_name', 'search_term', 'location', 'results_wanted', 'hours_old', 'country_indeed']
-    missing_params = [param for param in required_params if param not in event]
-    if len(missing_params) > 0:
-        return {
-            'statusCode': 400,
-            'body': f"Missing required parameters: {', '.join(missing_params)}"
-        }
 
-    try:
-        jobs = scrape_jobs(
-            site_name=[event['site_name']],
-            search_term=event['search_term'],
-            location=event['location'],
-            results_wanted=event['results_wanted'],
-            hours_old=event['hours_old'],
-            country_indeed=event['country_indeed']
-        )
-
-        # Convert DataFrame to dict for JSON serialization
-        jobs_dict = jobs.to_dict('records')
-        
-        # Convert datetime objects to ISO format strings
-        for job in jobs_dict:
-            for key, value in job.items():
-                if pd.isna(value):  # Handle NaN values
-                    job[key] = None
-                elif isinstance(value, pd.Timestamp) or hasattr(value, 'isoformat'):
-                    job[key] = value.isoformat()
-                    
-        # Send each job to EventBridge
-        # Claude Haiku 3.5 can handle 2000 req/min or 33 req/sec
-        # We want to limit the number of requests to 25 per second
-        logger.info(f"Sending {len(jobs_dict)} jobs to EventBridge")
-        for job in jobs_dict:
-            eventbridge.put_events(
-                Entries=[
-                    {
-                        'Source': 'job.scraper',
-                        'DetailType': 'JobScrapeEvent',
-                        'Detail': json.dumps({'job': job}),
-                        'EventBusName': 'JobScrapeEventBus'
-                    }
-                ]
+    scrape_params = get_scrape_params()
+    total_jobs = 0
+    err_count = 0
+    for params in scrape_params:
+        try:
+            jobs = scrape_jobs(
+                site_name=params['site_name'],
+                search_term=params['search_term'],
+                location=params['location'],
+                results_wanted=params['results_wanted'],
+                hours_old=params['hours_old'],
+                country_indeed=params['country_indeed']
             )
 
-        response = {
-            'statusCode': 200,
-            'body': f"{len(jobs_dict)} jobs found."
-        }
+            # Convert DataFrame to dict for JSON serialization
+            jobs_dict = jobs.to_dict('records')
+            
+            # Convert datetime objects to ISO format strings
+            for job in jobs_dict:
+                for key, value in job.items():
+                    if pd.isna(value):  # Handle NaN values
+                        job[key] = None
+                    elif isinstance(value, pd.Timestamp) or hasattr(value, 'isoformat'):
+                        job[key] = value.isoformat()
+                        
+            # Send each job to EventBridge
+            # Claude Haiku 3.5 can handle 2000 req/min or 33 req/sec
+            # We want to limit the number of requests to 25 per second
+            logger.info(f"Sending {len(jobs_dict)} jobs to EventBridge")
+            for job in jobs_dict:
+                eventbridge.put_events(
+                    Entries=[
+                        {
+                            'Source': 'job.scraper',
+                            'DetailType': 'JobScrapeEvent',
+                            'Detail': json.dumps({'job': job}),
+                            'EventBusName': 'JobScrapeEventBus'
+                        }
+                    ]
+                )
+            total_jobs += len(jobs_dict)
 
-        return response
+        except Exception as e:
+            logger.error(f"ERROR: scraping jobs with params: {params} \n ERROR: {e}")
+            err_count += 1
+            continue
 
-    except Exception as e:
-        return {
+
+
+    if err_count > 0:
+       return {
             'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
+            'body': f"{err_count} errors found."
         }
+
+    else:
+        return {
+            'statusCode': 200,
+            'body': f"{total_jobs} jobs found."
+        }
+
+
+
+    
