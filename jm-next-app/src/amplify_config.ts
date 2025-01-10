6bf857acc3113d9293c9d739a8c475a07a0ce2ec
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-09 20:19:00 -0800

    amplify config

--- jm-next-app/src/amplify_config.ts
+++ jm-next-app/src/amplify_config.ts
@@ -1,11 +1,27 @@
-import { Resource } from "sst";
-
 const config = {
-    cognito: {
-        region: Resource.Region.value,
-        userPoolId: Resource.UserPoolID.value,
-        userPoolWebClientId: Resource.UserPoolClientID.value,
-        identityPoolId: Resource.IdentityPoolID.value
+    Auth: {
+        Cognito: {  
+            userPoolId: String(process.env.NEXT_PUBLIC_USER_POOL_ID),
+            userPoolClientId: String(process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID),
+            identityPoolId: String(process.env.NEXT_PUBLIC_IDENTITY_POOL_ID),
+            loginWith: {
+                email: true,
+              },
+              signUpVerificationMethod: "code",
+              userAttributes: {
+                email: {
+                  required: true,
+                },
+              },
+              allowGuestAccess: true,
+              passwordFormat: {
+                minLength: 8,
+                requireLowercase: true,
+                requireUppercase: true,
+                requireNumbers: true,
+                requireSpecialCharacters: false,
+              },
+        }
     }
 }
 
