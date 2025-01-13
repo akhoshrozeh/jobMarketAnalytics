10ef4c1e03ed2215ecf1d578a38ae0b7f477fe20
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-12 23:49:47 -0800

    checks access toke

--- jm-next-app/src/middleware.ts
+++ jm-next-app/src/middleware.ts
@@ -1,9 +1,33 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
- 
-export function middleware(request: NextRequest) {
-  console.log('\n\n\nRequest URL:', request.url)
-  console.log('Request headers:', Object.fromEntries(request.headers))
-  console.log('Request method:', request.method)
+import { CognitoJwtVerifier } from 'aws-jwt-verify'
+
+// Create verifier once, outside the middleware function
+const verifier = CognitoJwtVerifier.create({
+  userPoolId: String(process.env.NEXT_PUBLIC_USER_POOL_ID),
+  tokenUse: "access",
+  clientId: String(process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID),
+});
+
+export async function middleware(request: NextRequest) {
+  const allCookies = Array.from(request.cookies.getAll());
+  const accessTokenCookie = allCookies.find(cookie => cookie.name.includes('accessToken'));
+
+  if (!accessTokenCookie) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  const accessToken: string = accessTokenCookie.value;
+  console.log("accessToken:", accessToken);
+
+  try {
+    const payload = await verifier.verify(accessToken);
+    console.log("Token is valid. Payload:", payload);
+  } catch {
+    console.log("Token not valid!");
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  
+  
   return NextResponse.next()
 }
