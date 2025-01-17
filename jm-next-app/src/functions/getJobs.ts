9cd6b4bf9be5b3cbfd5deefaf0ff7ec1d2361ea6
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-17 08:31:13 -0800

    projection to reduce res size

--- jm-next-app/src/functions/getJobs.ts
+++ jm-next-app/src/functions/getJobs.ts
@@ -9,12 +9,29 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
 
     try {
         // Temporarily only getting jobs from indeed
-        const result = await db.collection('JobPostings').find({"site": "indeed"}).sort({date_posted: -1}).limit(100).toArray();
+        const result = await db
+        .collection('JobPostings')
+        .find({"site": "indeed"},
+            {
+                projection: {
+                    _id: 1,
+                    title: 1,
+                    company: 1,
+                    location: 1,
+                    date_posted: 1,
+                    site: 1
+                }
+            }
+        )
+        .sort({date_posted: -1})
+        .limit(100)
+        .toArray();
 
         return {
             statusCode: 200,
             headers: {
               "Content-Type": "application/json",
+              "Cache-Control": "public, max-age=3600"
             },
             body: JSON.stringify(result)
         };
