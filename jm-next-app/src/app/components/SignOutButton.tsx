f01ddc9d598a21c067250c2353d4d16afee9a831
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-16 23:51:35 -0800

    rm amplify configure

--- jm-next-app/src/app/components/SignOutButton.tsx
+++ jm-next-app/src/app/components/SignOutButton.tsx
@@ -1,24 +1,14 @@
 "use client"
 
 import { signOut } from "aws-amplify/auth";
-import { Amplify } from "aws-amplify";
-import config from "../../amplify_config";
 import { Button } from "@headlessui/react";
-import { useRouter } from "next/navigation";
-
-
-Amplify.configure(config as any, {
-    ssr: true
-});
 
 export default function SignOutButton() {
-    const router = useRouter();
-
     const handleSignOut = async () => {
         await signOut();
         window.location.href = '/';
-
     }
+    
     return (
         <Button
             className="w-full text-left"
