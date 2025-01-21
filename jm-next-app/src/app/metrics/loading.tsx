7b57257621e073f90dc12697717d7d087391ab8a
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-21 10:41:47 -0800

    metrics sidebar w separate graph routes

---
+++ jm-next-app/src/app/metrics/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return <div>
+        <div className="flex justify-center items-center flex-col p-8 text-white">
+            <h1 className="text-2xl font-bold mb-4 mt-32">Loading Metrics data...</h1>
+        </div>
+    </div>
+}
