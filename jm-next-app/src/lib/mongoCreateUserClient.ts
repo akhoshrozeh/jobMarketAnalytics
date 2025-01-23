89582dc338e00a7203433d8226ed14fd81855513
akhoshrozeh <akhoshrozeh@gmail.com>
2025-01-22 23:21:56 -0800

    creates connection under least privilege user

---
+++ jm-next-app/src/lib/mongoCreateUserClient.ts
@@ -0,0 +1,22 @@
+import { Resource } from "sst"
+import * as mongodb from "mongodb";
+
+const MongoClient = mongodb.MongoClient;
+const mongoURI = Resource.CreateUserURI.value;
+const db = Resource.JMDatabase.value;
+
+let cachedDb: mongodb.Db | null = null;
+
+export async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  // Connect to our MongoDB database hosted on MongoDB Atlas
+  const client = await MongoClient.connect(mongoURI);
+
+  // Specify which database we want to use
+  cachedDb = await client.db(db);
+
+  return cachedDb;
+}
