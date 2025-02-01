035d618e80d9e0d7e81616bbf82dabeb49a586f7
akhoshrozeh <akhoshrozeh@gmail.com>
2025-02-01 14:25:10 -0800

    get new tokens after payment:

---
+++ jm-next-app/src/app/confirmation/page.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { useEffect } from "react";
+import { fetchAuthSession } from "aws-amplify/auth";
+import Link from "next/link";
+
+export default function Confirmation() {
+    useEffect(() => {
+        const refreshTokens = async () => {
+          try {
+            await fetchAuthSession({ forceRefresh: true });
+          } catch (error) {
+            console.error('Error refreshing tokens:', error);
+          }
+        };
+        refreshTokens();
+      }, []);
+    
+      return (
+        <div className="mx-16">
+            <h1 className="flex flex-col justify-center text-center mt-32 
+              text-lg    
+              md:text-xl  
+              lg:text-2xl 
+              xl:text-3xl 
+              2xl:text-4xl"> 
+                Thank you for purchasing a membership to JobTrendr!
+            </h1>
+           
+            <Link 
+              href="/metrics/top-skills" 
+              className="flex flex-col justify-center text-center mt-16 
+                text-base   
+                md:text-lg  
+                lg:text-xl  
+                max-w-sm md:max-w-md mx-auto "
+            >
+                <button className="bg-emerald-500/80 hover:bg-emerald-500 text-white px-4 py-2 rounded-md
+                  text-sm     
+                  md:text-base 
+                  lg:text-lg"> 
+                    Get Started
+                </button>
+            </Link>
+            
+        </div>
+      );
+}
