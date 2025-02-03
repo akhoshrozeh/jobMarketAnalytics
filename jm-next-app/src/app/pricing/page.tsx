1fe5b925b1112da5b95229ca1cdf35bc75365bf5
akhoshrozeh <akhoshrozeh@gmail.com>
2025-02-02 17:20:34 -0800

    update logic

--- jm-next-app/src/app/pricing/page.tsx
+++ jm-next-app/src/app/pricing/page.tsx
@@ -4,6 +4,7 @@ import { CheckIcon } from '@heroicons/react/20/solid'
 import { verifyIdToken } from '../../utils/verifyToken'
 import { Stripe } from 'stripe';
 import { Resource } from 'sst';
+import { CognitoIdTokenPayload } from 'aws-jwt-verify/jwt-model';
 
 const stripeSecretKey = Resource.STRIPE_SECRET_KEY.value;
 const stripe = new Stripe(stripeSecretKey)
@@ -52,56 +53,89 @@ function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
 }
 
+async function createBasicCheckout(idToken: CognitoIdTokenPayload | null) {
+  if (!idToken) {
+    return null;
+  }
+
+  const basicPaymentLink = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: basicPriceId,
+        quantity: 1,
+      },
+    ],
+    metadata: {
+      metadata_email: String(idToken.email),
+      metadata_username: String(idToken.sub)
+    },
+    success_url: (process.env.NODE_ENV === "development" ? "http://localhost:3000/confirmation" : "https://jobtrender.com/confirmation"),
+    mode: "payment"
+  });
+  
+  return basicPaymentLink
+}
+
+async function createPremiumCheckout(idToken: CognitoIdTokenPayload | null) {
+  if (!idToken) {
+    return null;
+  }
+  
+  console.log("here")
+  const premiumPaymentLink = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: premiumPriceId,
+        quantity: 1
+      }
+    ],
+    metadata: {
+      metadata_email: String(idToken.email),
+      metadata_username: String(idToken.sub)
+    },
+    success_url: (process.env.NODE_ENV === "development" ? "http://localhost:3000/confirmation" : "https://jobtrender.com/confirmation"),
+    mode: "payment"        
+  })
+
+  console.log("premiumPaymentLink: ", premiumPaymentLink)
+
+  return premiumPaymentLink
+
+}
+
 export default async function Pricing() {
 
   // Returns null if invalid token or dne
   const idToken = await verifyIdToken();
-  // let userTier = 'free';
-  // if (idToken) {
-  //   userTier = String(idToken["custom:tier"])
-  //   console.log("userTier", userTier)
-  // }
-  // console.log("idToken: ", idToken)
 
+  let userTier = 'free';
   if (idToken) {
-      const basicPaymentLink = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price: basicPriceId,
-            quantity: 1,
-          },
-        ],
-        metadata: {
-          metadata_email: String(idToken?.email),
-          metadata_username: String(idToken?.sub)
-        },
-        success_url: (process.env.NODE_ENV === "development" ? "http://localhost:3000/confirmation" : "https://jobtrender.com/confirmation"),
-        mode: "payment"
-        
-        
-        
-      });
-      
-      const premiumPaymentLink = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price: premiumPriceId,
-            quantity: 1
-          }
-        ],
-        metadata: {
-          metadata_email: String(idToken?.email),
-          metadata_username: String(idToken?.sub)
-        },
-        success_url: (process.env.NODE_ENV === "development" ? "http://localhost:3000/confirmation" : "https://jobtrender.com/confirmation"),
-        mode: "payment"        
-      })
-
-
-
-    tiers[0].href = basicPaymentLink.url || "" // + `?prefilled_email=${encodeURIComponent(String(idToken?.email))}`;
-    tiers[1].href = premiumPaymentLink.url || ""// + `?prefilled_email=${encodeURIComponent(String(idToken?.email))}`;
+    userTier = String(idToken["custom:tier"])
   }
+
+  if (!idToken) {
+    tiers[0].href = "/sign-up";
+    tiers[1].href = "/sign-up";
+  }
+  
+  else {
+    if (userTier == "free") {
+      const basicPaymentLink = await createBasicCheckout(idToken)
+      const premiumPaymentLink = await createPremiumCheckout(idToken)
+      console.log("basicPaymentLink: ", basicPaymentLink)
+      tiers[0].href = basicPaymentLink?.url + `?prefilled_email=${encodeURIComponent(String(idToken?.email))}`;
+      tiers[1].href = premiumPaymentLink?.url + `?prefilled_email=${encodeURIComponent(String(idToken?.email))}`;
+    }
+    else if (userTier == "basic") {
+      const premiumPaymentLink = await createPremiumCheckout(idToken)
+      tiers[1].href = premiumPaymentLink?.url || "" // + `?prefilled_email=${encodeURIComponent(String(idToken?.email))}`;
+      tiers[0].href = ""; // Disable basic tier
+    } else { // premium user
+      tiers[0].href = "";
+      tiers[1].href = "";
+    }
+  }
+
 
   return (
     <div className="py-24 sm:py-32">
@@ -152,18 +186,20 @@ export default async function Pricing() {
                 <span className="text-4xl font-semibold tracking-tight text-white">{tier.price}</span>
                 {/* <span className="text-sm/6 font-semibold text-gray-300"></span> */}
               </p>
-              <a
-                href={tier.href}
-                aria-describedby={tier.id}
-                className={classNames(
-                  tier.mostPopular
-                    ? 'bg-emerald-500 text-white shadow-sm hover:bg-emerald-400 focus-visible:outline-emerald-500'
-                    : 'bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white',
-                  'mt-6 block rounded-md px-3 py-2 text-center text-sm/6 font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2',
-                )}
-              >
-                Buy plan
-              </a>
+              
+                <a
+                  href={tier.href}
+                  aria-describedby={tier.id}
+                  className={classNames(
+                    tier.mostPopular
+                      ? 'bg-emerald-500 text-white shadow-sm hover:bg-emerald-400 focus-visible:outline-emerald-500'
+                      : 'bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white',
+                    'mt-6 block rounded-md px-3 py-2 text-center text-sm/6 font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2',
+                  )}
+                >
+                  Buy plan
+                </a>
+              
               <ul role="list" className="mt-8 space-y-3 text-sm/6 text-gray-300 xl:mt-10">
                 {tier.features.map((feature) => (
                   <li key={feature} className="flex gap-x-3">
