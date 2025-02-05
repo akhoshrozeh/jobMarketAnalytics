1e9db9a7a4db2237e9f4de0eb1c1657294909988
akhoshrozeh <akhoshrozeh@gmail.com>
2025-02-05 13:58:30 -0800

    rm image

--- jm-next-app/src/app/page.tsx
+++ jm-next-app/src/app/page.tsx
@@ -49,13 +49,13 @@ export default async function Home() {
         </div>
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-            <Image
+            {/* <Image
               alt="App screenshot"
               src="https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot.png"
               width={2432}
               height={1442}
-              className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
-            />
+              className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10 overflow-x-hidden"
+            /> */}
           </div>
         </div>
       </div>
