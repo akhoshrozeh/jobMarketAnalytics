9c820c411c5507e9f61b3aa90f0cbb353c41f838
akhoshrozeh <akhoshrozeh@gmail.com>
2025-02-08 13:39:56 -0800

    remove old infra; add table and bucket

--- job-market-cdk/job_market_cdk/job_market_cdk_stack.py
+++ job-market-cdk/job_market_cdk/job_market_cdk_stack.py
@@ -6,6 +6,7 @@ from aws_cdk import (
     aws_events as events,
     aws_events_targets as targets, 
     aws_iam as iam,
+    dynamodb,
     aws_secretsmanager as sm,
     Duration, 
     CfnOutput,
@@ -15,7 +16,7 @@ from aws_cdk import (
 from constructs import Construct
 import boto3
 import json
-from utils.secrets import get_mongodb_uri, get_db_collection
+from utils.secrets import get_mongodb_uri, get_db_collection, get_openai_api_key
 from botocore.exceptions import ClientError
 
 
@@ -25,19 +26,54 @@ class JobMarketCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # ********
+        # ************************************************************
+        # *                                                          *
+        # *                         Storage                          * 
+        # *                                                          *
+        # ************************************************************
+
+        # From the previous stack architeture
         s3_bucket = _s3.Bucket(self, "JobPostingsRawData", 
             block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
         )
 
+        batch_bucket = _s3.Bucket(self, "JobPostsBatches", 
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL,
+            versioned=True,
+            lifecycle_rules=[
+                _s3.LifecycleRule(
+                    transitions=[
+                        _s3.Transition(
+                            storage_class=_s3.StorageClass.INTELLIGENT_TIERING,
+                            transition_after=Duration.days(0)
+                        ),
+
+                    ],
+                    enabled=True
+                )
+            ]
+        )
+
+        batch_jobs_table = dynamodb.Table(self, "BatchJobsTable",
+            partition_key=dynamodb.Attribute(name="batch_id", type=dynamodb.AttributeType.STRING),
+            sort_key=dynamodb.Attribute(name="batch_id", type=dynamodb.AttributeType.STRING),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
+        
+        
+
+        batch_bucket.grant_write(sync_s3_mongo_lambda)
+
+        # ************************************************************
+        # *                                                          *
+        # *                         Secrets                          * 
+        # *                                                          *
+        # ************************************************************
+
         mongodb_uri_secret = get_mongodb_uri()
         mongodb_db, mongodb_collection = get_db_collection()
+        openai_api_key_secret = get_openai_api_key()
 
-     # Create EventBridge event bus
-        event_bus = events.EventBus(self, "JobScrapeEventBus", event_bus_name="JobScrapeEventBus")
-
-        bedrock_queue = sqs.Queue(self, "BedrockProcessorQueue", queue_name="BedrockProcessorQueue", visibility_timeout=Duration.minutes(5))
-        # bedrock_dlq = sqs.DeadLetterQueue(self, "BedrockProcessorDLQ", queue=bedrock_queue)
         # ************************************************************
         # *                                                          *
         # *                   Lambda Layers                          * 
@@ -82,14 +118,11 @@ class JobMarketCdkStack(Stack):
                 "MONGODB_URI": mongodb_uri_secret,
                 "MONGODB_DATABASE": mongodb_db,
                 "MONGODB_COLLECTION": mongodb_collection, 
-                "EVENT_BUS_NAME": event_bus.event_bus_name,
-                "QUEUE_URL": bedrock_queue.queue_url,   
             },
             timeout=Duration.minutes(5),
             memory_size=512
         )
 
-        bedrock_queue.grant_send_messages(sync_s3_mongo_lambda) 
 
         s3_bucket.grant_read(sync_s3_mongo_lambda)
 
@@ -99,8 +132,8 @@ class JobMarketCdkStack(Stack):
             handler="jobspy_scrapers.handler",
             code=_lambda.Code.from_asset("lambda"),
             layers=[jobspy_layer, boto3_layer],
-              environment={
-                "EVENT_BUS_NAME": event_bus.event_bus_name
+            environment={
+                "OPENAI_API_KEY": openai_api_key_secret
             },
             timeout=Duration.minutes(5),
             memory_size=512
@@ -118,7 +151,6 @@ class JobMarketCdkStack(Stack):
             code=_lambda.Code.from_asset("lambda"),
             environment={
                 "BUCKET_NAME": s3_bucket.bucket_name,
-                "QUEUE_URL": bedrock_queue.queue_url,   
             },
             timeout=Duration.minutes(5),
             memory_size=512
@@ -128,50 +160,7 @@ class JobMarketCdkStack(Stack):
         s3_bucket.grant_write(s3_writer_lambda)
 
 
-        bedrock_queue.grant_send_messages(s3_writer_lambda)
 
-
-
-          # Add Bedrock permissions
-        bedrock_access_policy = iam.PolicyStatement(
-            effect=iam.Effect.ALLOW,
-            actions=[
-                "bedrock:InvokeModel",
-                "bedrock:InvokeModelWithResponseStream",
-                "bedrock:ListFoundationModels"
-            ],
-            resources=["*"]  # You might want to restrict this to specific model ARNs
-        )
-         # Create the IAM role for Lambda
-        bedrock_access_role = iam.Role(
-            self, 
-            "BedrockAccessRole",
-            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com")
-        )
-        
-        # Add the policy to the role
-        bedrock_access_role.add_to_policy(bedrock_access_policy)
-
-        # Create Bedrock processing Lambda
-        bedrock_processor_lambda = _lambda.Function(
-            self,
-            "BedrockProcessorFunction",
-            runtime=_lambda.Runtime.PYTHON_3_12,
-            handler="bedrock_processor.handler",
-            code=_lambda.Code.from_asset("lambda"),
-            role=bedrock_access_role,
-            environment={
-                "QUEUE_URL": bedrock_queue.queue_url,
-                "EVENT_BUS_NAME": event_bus.event_bus_name
-            },
-            timeout=Duration.minutes(5),
-            memory_size=512,
-            reserved_concurrent_executions=1
-        )
-
-        bedrock_queue.grant_consume_messages(bedrock_processor_lambda)
-        bedrock_processor_lambda.add_event_source(lambda_event_source.SqsEventSource(bedrock_queue, batch_size=1))
-       
 
         # Create MongoDB writer Lambda
         mongodb_writer_lambda = _lambda.Function(
@@ -195,99 +184,12 @@ class JobMarketCdkStack(Stack):
 
         
 
-         # Create EventBridge rule to trigger S3 writer Lambda
-        sync_s3_mongo_rule = events.Rule(self, "SyncS3MongoRule",
-            event_bus=event_bus,
-            event_pattern=events.EventPattern(
-                source=["sync_s3_mongo"],
-                detail_type=["SyncS3MongoEvent"]
-            )
-        )
-        sync_s3_mongo_rule.add_target(targets.LambdaFunction(bedrock_processor_lambda))
-       
-        
-        
-
-
-
-
-
-        # Create EventBridge rule to trigger S3 writer Lambda
-        s3_writer_rule = events.Rule(self, "S3WriterRule",
-            event_bus=event_bus,
-            event_pattern=events.EventPattern(
-                source=["job.scraper"],
-                detail_type=["JobScrapeEvent"]
-            )
-        )
-        s3_writer_rule.add_target(targets.LambdaFunction(s3_writer_lambda))
-        # Add Lambda invoke permissions for EventBridge
-        s3_writer_lambda.add_permission(
-            "EventBridgeInvoke",
-            principal=iam.ServicePrincipal("events.amazonaws.com"),
-            action="lambda:InvokeFunction",
-            source_arn=s3_writer_rule.rule_arn
-        )
-
-
-        
-        # Create EventBridge rule to trigger Bedrock processor Lambda
-        bedrock_processor_rule = events.Rule(self, "BedrockProcessorRule",
-            event_bus=event_bus,
-            event_pattern=events.EventPattern(
-                source=["s3.writer"],
-                detail_type=["S3WriteCompleteEvent"]
-            )
-        )
-
-        bedrock_processor_rule.add_target(targets.LambdaFunction(bedrock_processor_lambda))
-        bedrock_processor_lambda.add_permission(
-            "EventBridgeInvoke",
-            principal=iam.ServicePrincipal("events.amazonaws.com"),
-            action="lambda:InvokeFunction",
-            source_arn=bedrock_processor_rule.rule_arn
-        )
-
-
-
-
-        # Trigger mongodb writer lambda
-        mongodb_writer_rule = events.Rule(self, "MongoDBWriterRule",
-            event_bus=event_bus,
-            event_pattern=events.EventPattern(
-                source=["bedrock.processor"],
-                detail_type=["BedrockProcessedEvent"]
-            )
-        )
-        mongodb_writer_rule.add_target(targets.LambdaFunction(mongodb_writer_lambda))
-        mongodb_writer_lambda.add_permission(
-            "EventBridgeInvoke",
-            principal=iam.ServicePrincipal("events.amazonaws.com"),
-            action="lambda:InvokeFunction",
-            source_arn=mongodb_writer_rule.rule_arn
-        )
 
         # Grant lambdas to put events on the event bus
         event_policy = iam.PolicyStatement(effect=iam.Effect.ALLOW, resources=['*'], actions=['events:PutEvents'])
         scrape_jobs_lambda.add_to_role_policy(event_policy)
-        bedrock_processor_lambda.add_to_role_policy(event_policy)
         s3_writer_lambda.add_to_role_policy(event_policy)
         sync_s3_mongo_lambda.add_to_role_policy(event_policy)
-
-        # You can keep the existing grant_put_events_to line as well
-        event_bus.grant_put_events_to(s3_writer_lambda)
-
-        # Grant permissions to put events on the    t bus
-        event_bus.grant_put_events_to(scrape_jobs_lambda)
-
-        # Grant permissions to the Bedrock processor Lambda to put events
-        event_bus.grant_put_events_to(bedrock_processor_lambda)
-
-        # Grant permissions to the MongoDB writer Lambda to put events (if needed)
-        event_bus.grant_put_events_to(mongodb_writer_lambda)
-
-        event_bus.grant_put_events_to(sync_s3_mongo_lambda)
-
 
 
         # #################### SCHEDULE ###############################
@@ -323,7 +225,6 @@ class JobMarketCdkStack(Stack):
         # Add logging configuration for each Lambda
         scrape_jobs_lambda.add_to_role_policy(cloudwatch_policy)
         s3_writer_lambda.add_to_role_policy(cloudwatch_policy)
-        bedrock_processor_lambda.add_to_role_policy(cloudwatch_policy)
         mongodb_writer_lambda.add_to_role_policy(cloudwatch_policy)
 
         # Add CloudWatch Metrics permissions
@@ -338,21 +239,13 @@ class JobMarketCdkStack(Stack):
         )
 
         # Add metrics permissions to all Lambdas
-        for lambda_func in [scrape_jobs_lambda, s3_writer_lambda, bedrock_processor_lambda, mongodb_writer_lambda]:
+        for lambda_func in [scrape_jobs_lambda, s3_writer_lambda, mongodb_writer_lambda]:
             lambda_func.add_to_role_policy(metrics_policy)
 
         # Create CloudWatch Log group outputs for easy reference
         CfnOutput(self, "ScrapeJobsLogGroup", value=scrape_jobs_lambda.log_group.log_group_name)
         CfnOutput(self, "S3WriterLogGroup", value=s3_writer_lambda.log_group.log_group_name)
-        CfnOutput(self, "BedrockProcessorLogGroup", value=bedrock_processor_lambda.log_group.log_group_name)
         CfnOutput(self, "MongoDBWriterLogGroup", value=mongodb_writer_lambda.log_group.log_group_name)
-        CfnOutput(self, "EventBusName", value=event_bus.event_bus_name)
-
-
-
-
-# FRONT END: diff stack
-# api gateway + lambda: for querying mongodb
 
 
 
