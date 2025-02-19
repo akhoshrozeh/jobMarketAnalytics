e1a8782c686318db358d049c853a7c6be83fc5ba
akhoshrozeh <akhoshrozeh@gmail.com>
2025-02-19 00:57:45 -0800

    rm jobs page

--- jm-next-app/src/app/jobs/layout.tsx
+++
@@ -1,6 +0,0 @@
-export default function JobsLayout({ children }: { children: React.ReactNode }) {
-
-  return <div>
-    {children}
-    </div>;
-}
--- jm-next-app/src/app/jobs/loading.tsx
+++
@@ -1,5 +0,0 @@
-import JobSkeleton from "./JobsSkeleton";
-
-export default function Loading() {
-    return <JobSkeleton />;
-}
