34f7e194bb517c6db458b0a1979cd9b15a5d8a51
akhoshrozeh <akhoshrozeh@gmail.com>
2025-03-02 02:28:15 -0800

    update f0nts

--- jm-next-app/src/app/sign-up/page.tsx
+++ jm-next-app/src/app/sign-up/page.tsx
@@ -165,7 +165,7 @@ export default function SignUp() {
               className="mx-auto h-10 w-auto"
             /> */}
 
-            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Create an account</h2>
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-black">Create an account</h2>
           </div>
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm md:max-w-md">
@@ -176,7 +176,7 @@ export default function SignUp() {
             { waitingForConfirmation == false ? (            
                 <form onSubmit={handleSubmit} className="space-y-6">
                 <div>
-                    <label htmlFor="email" className="block text-md/6 font-medium text-white">
+                    <label htmlFor="email" className="block text-md/6 font-medium text-black">
                     Email address
                     </label>
                     <div className="mt-2">
@@ -186,14 +186,14 @@ export default function SignUp() {
                         type="email"
                         required
                         autoComplete="email"
-                        className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-500 sm:text-sm/6"
+                        className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline outline-1 -outline-offset-1 outline-black placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-500 sm:text-sm/6"
                     />
                     </div>
                 </div>
     
                 <div>
                     <div className="flex items-center justify-between">
-                    <label htmlFor="password" className="block text-md font-medium text-white">
+                    <label htmlFor="password" className="block text-md font-medium text-black">
                         Password
                     </label>
                     
@@ -205,7 +205,7 @@ export default function SignUp() {
                         type="password"
                         required
                         autoComplete="current-password"
-                        className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-500 sm:text-sm/6"
+                        className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline outline-1 -outline-offset-1 outline-black placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-500 sm:text-sm/6"
                     />
                     </div>
                 </div>
@@ -213,7 +213,7 @@ export default function SignUp() {
                 <div>
                     <button
                     type="submit"
-                    className="flex w-full justify-center rounded-md bg-emerald-500 px-3 py-1.5 text-md font-semibold text-white shadow-sm hover:bg-emerald-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500"
+                    className="flex w-full justify-center rounded-md bg-emerald-500 px-3 py-1.5 text-md font-semibold text-black shadow-sm hover:bg-emerald-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500"
                     >
                         { isLoading ? <LoadingSpinner/> : <>Sign Up</> }
                     </button>
@@ -226,15 +226,15 @@ export default function SignUp() {
                 Please check your email and enter the confirmation code.
                 <form onSubmit={handleConfirmation} className="space-y-6">
                   <div>
-                    <label htmlFor="code" className="block text-md mb-4 font-medium text-white">
+                    <label htmlFor="code" className="block text-md mb-4 font-medium text-black">
                       {/* Confirmation Code */}
                     </label>
-                    <input id="code" name="code" type="text" required autoComplete="confirmation-code" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-500 sm:text-sm/6" />
+                    <input id="code" name="code" type="text" required autoComplete="confirmation-code" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline outline-1 -outline-offset-1 outline-black placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-500 sm:text-sm/6" />
                   </div>
                   <div>
                   <button
                     type="submit"
-                    className="flex w-full justify-center rounded-md bg-emerald-500 px-3 py-1.5 text-md font-semibold text-white shadow-sm hover:bg-emerald-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500"
+                    className="flex w-full justify-center rounded-md bg-emerald-500 px-3 py-1.5 text-md font-semibold text-black shadow-sm hover:bg-emerald-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500"
                     >
                      { isLoading ? <LoadingSpinner/> : <>Confirm</> }
                     </button>
