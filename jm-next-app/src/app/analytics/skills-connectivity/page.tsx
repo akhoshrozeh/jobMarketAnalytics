565ae58956aa129f1d86febfdc896efe35dd29fc
akhoshrozeh <akhoshrozeh@gmail.com>
2025-03-08 23:33:14 -0800

    less nodes

--- jm-next-app/src/app/analytics/skills-connectivity/page.tsx
+++ jm-next-app/src/app/analytics/skills-connectivity/page.tsx
@@ -16,7 +16,7 @@ export default async function SkillsConnectivity() {
         <div className="flex justify-center items-center flex-col p-8 text-black">
             <h1 className="text-2xl font-bold mb-4">Relationship between Skills</h1>
             <div className="border rounded-lg pt-4 px-4 pb-2 border-m-dark-green border-2"> 
-                {<KeywordsConnectedByJob nodes={aggGroup.slice(0, 100)} links={keywordsConnectedByJob}/>}
+                {<KeywordsConnectedByJob nodes={aggGroup.slice(0, 20) as any} links={keywordsConnectedByJob as any}/>}
             </div>
         </div>
     );
