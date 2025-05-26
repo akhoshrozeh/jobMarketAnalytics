fea8954df1f3a7d4e95bd811162a13b80873de6b
akhoshrozeh <akhoshrozeh@gmail.com>
2025-05-26 21:05:56 +0800

    add metrics; get topskills only if paid usr

--- jm-next-app/src/app/analytics/skills/layout.tsx
+++ jm-next-app/src/app/analytics/skills/layout.tsx
@@ -2,6 +2,7 @@ import { getTopSkillsClient, getTier, getSkillData } from '@/lib/dataAcessLayer'
 import { SkillProvider } from './SkillContext'
 import SearchContainer from './SearchContainer'
 import BentoLayout from './bentoLayout'
+import UpgradeModal from './UpgradeModal'
 
 // Server Action for fetching skill data
 async function fetchSkillData(skill: string) {
@@ -13,27 +14,31 @@ export default async function Layout({
   children,
   TopSkills,
   TotalJobs,
-  Salary
+  Salary,
+  Grade,
+  TopJobs
 }: {
   children: React.ReactNode
   TopSkills: React.ReactNode
   TotalJobs: React.ReactNode
   Salary: React.ReactNode
+  Grade: React.ReactNode
+  TopJobs: React.ReactNode
 }) {
   // Fetch searchable skills during SSR
   const tier = await getTier();
-  const skills = await getTopSkillsClient(tier);
+  const skills = tier === "free" ? [] : await getTopSkillsClient(tier);
 
   return (
-    <SkillProvider>
+    <SkillProvider initialTier={tier}>
       <div>
         <h1 className="text-3xl font-bold text-center mb-6">Skills Research</h1>
         <SearchContainer skills={skills} fetchSkillData={fetchSkillData}>
           {TopSkills}
         </SearchContainer>
 
-        <BentoLayout TotalJobs={TotalJobs} Salary={Salary} />
-
+        <BentoLayout TotalJobs={TotalJobs} Salary={Salary} Grade={Grade} TopJobs={TopJobs} />
+        <UpgradeModal />
       </div>
     </SkillProvider>
   )
