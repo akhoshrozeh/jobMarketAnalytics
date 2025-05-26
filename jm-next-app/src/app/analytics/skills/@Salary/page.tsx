a05c45d879d726c85df802f871607a67f6015afa
akhoshrozeh <akhoshrozeh@gmail.com>
2025-05-26 21:03:23 +0800

    update props

--- jm-next-app/src/app/analytics/skills/@Salary/page.tsx
+++ jm-next-app/src/app/analytics/skills/@Salary/page.tsx
@@ -3,11 +3,11 @@ import { useSkill } from "../SkillContext";
 import { SalaryDistributionGraph } from "../SkillsPageGraphs";
 
 export default function TotalJobs() {
-    const { skillData } = useSkill();
+    const { skillData, selectedSkill } = useSkill();
     console.log("skillData", skillData)
     return (
         <div className="text-center text-xl">
-            <SalaryDistributionGraph minSalaries={skillData?.averageSalaryForSkill.minSalaries} maxSalaries={skillData?.averageSalaryForSkill.maxSalaries} />
+            <SalaryDistributionGraph selectedSkill={selectedSkill as string} totalJobs={skillData?.salaryDistributionForSkill.totalJobs[0].total} minSalaries={skillData?.salaryDistributionForSkill.minSalaries} maxSalaries={skillData?.salaryDistributionForSkill.maxSalaries} />
         </div>
     )
 }
