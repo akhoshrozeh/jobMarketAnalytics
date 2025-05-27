a55a73a48fc2bf2f63ecd46567b14d4ed42c291a
akhoshrozeh <akhoshrozeh@gmail.com>
2025-05-27 21:56:03 +0800

    styling

--- jm-next-app/src/app/analytics/skills/@RelatedSkills/page.tsx
+++ jm-next-app/src/app/analytics/skills/@RelatedSkills/page.tsx
@@ -6,14 +6,14 @@ export default function RelatedSkills() {
     
     return (
         <div className="p-4">
-            <div className="max-h-[400px] overflow-y-auto rounded-lg border border-gray-200 shadow-sm">
+            <div className="max-h-[400px] overflow-y-auto rounded-lg border border-gray-200 shadow-sm bg-white/60">
                 <div className="divide-y divide-gray-200">
                     {skillData?.relatedSkills.map((skill: any, index: number) => {
                         const percentage = ((skill.count / skillData.totalJobsForSkill) * 100).toFixed(1);
                         return (
                             <div 
                                 key={skill.keyword}
-                                className="p-3 hover:bg-gray-50 transition-colors duration-150 flex items-center"
+                                className="p-3 hover:bg-m-dark-green/30 transition-colors duration-150 flex items-center"
                             >
                                 <span className="w-8 text-gray-500 font-medium text-sm sm:text-base">#{index + 1}</span>
                                 <span className="font-medium text-gray-900 flex-grow text-sm sm:text-base">{skill.keyword}</span>
