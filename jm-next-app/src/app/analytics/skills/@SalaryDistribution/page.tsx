0944c5045185a8977c46b19f0afbd22c821e95e5
akhoshrozeh <akhoshrozeh@gmail.com>
2025-05-29 22:40:54 +0800

    formating

--- jm-next-app/src/app/analytics/skills/@SalaryDistribution/page.tsx
+++ jm-next-app/src/app/analytics/skills/@SalaryDistribution/page.tsx
@@ -4,10 +4,15 @@ import { SalaryDistributionGraph } from "../SkillsPageGraphs";
 
 export default function SalaryDistribution() {
     const { skillData, selectedSkill } = useSkill();
-    console.log("skillData", skillData)
+
     return (
         <div className="text-center text-xl">
-            <SalaryDistributionGraph selectedSkill={selectedSkill as string} totalJobs={skillData?.salaryDistributionForSkill.totalJobs[0].total} minSalaries={skillData?.salaryDistributionForSkill.minSalaries} maxSalaries={skillData?.salaryDistributionForSkill.maxSalaries} />
+            <SalaryDistributionGraph 
+                selectedSkill={selectedSkill as string} 
+                totalJobs={skillData.salaryDistributionForSkill.totalJobs[0].total} 
+                minSalaries={skillData.salaryDistributionForSkill.minSalaries} 
+                maxSalaries={skillData.salaryDistributionForSkill.maxSalaries} 
+            />
         </div>
     )
 }
