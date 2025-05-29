7bc2c00da3ac2c467e596a44c0e9a57c552e3236
akhoshrozeh <akhoshrozeh@gmail.com>
2025-05-29 19:51:11 +0800

    canonicalize keywords and convert strings to doubles for salary attrs

--- job-market-cdk/lambda/sanitize_mongo.py
+++ job-market-cdk/lambda/sanitize_mongo.py
@@ -1,4 +1,3 @@
-
 import pymongo
 import boto3
 from pymongo import UpdateMany
@@ -33,27 +32,178 @@ def handler(event, context):
     except Exception as e:
         logger.error(f"Error converting string to double: {e}")
         return {"status": "ERROR", "message": str(e)}
+    
+    try:
+        canonicalize_extracted_keywords(collection)
+    except Exception as e:
+        logger.error(f"Error canonicalizing extracted keywords: {e}")
+        return {"status": "ERROR", "message": str(e)}
 
     return {"status": "complete", "message": "Cleaned MongoDB values."}
 
 
 def convert_string_to_double(collection):
-    # Convert all min_amount and max_amount values from string to double
-    for document in collection.find({"$or": [{"min_amount": {"$type": "string"}}, {"max_amount": {"$type": "string"}}]}):
-        logger.info(f"Document: {document['min_amount']}")
-        # update_fields = {}
+    # Query to find documents with string min_amount or max_amount
+    query = {"$or": [{"min_amount": {"$type": "string"}}, {"max_amount": {"$type": "string"}}]}
+    
+    # Iterate over the documents and perform the conversion
+    count = 0
+    for document in collection.find(query):
+        logger.info(f"Processing document ID: {document['_id']}")
+        update_fields = {}
         
-        # if isinstance(document.get("min_amount"), str):
-        #     try:
-        #         update_fields["min_amount"] = float(document["min_amount"])
-        #     except ValueError:
-        #         logger.warning(f"Could not convert min_amount to float for document ID {document['_id']}")
+        # Convert min_amount to float if it's a string
+        if isinstance(document.get("min_amount"), str):
+            try:
+                update_fields["min_amount"] = float(document["min_amount"])
+            except ValueError:
+                logger.warning(f"Could not convert min_amount to float for document ID {document['_id']}")
         
-        # if isinstance(document.get("max_amount"), str):
-        #     try:
-        #         update_fields["max_amount"] = float(document["max_amount"])
-        #     except ValueError:
-        #         logger.warning(f"Could not convert max_amount to float for document ID {document['_id']}")
+        # Convert max_amount to float if it's a string
+        if isinstance(document.get("max_amount"), str):
+            try:
+                update_fields["max_amount"] = float(document["max_amount"])
+            except ValueError:
+                logger.warning(f"Could not convert max_amount to float for document ID {document['_id']}")
         
-        # if update_fields:
-        #     collection.update_one({"_id": document["_id"]}, {"$set": update_fields})
+        # Update the document if there are fields to update
+        if update_fields:
+            collection.update_one({"_id": document["_id"]}, {"$set": update_fields})
+            logger.info(f"Updated document ID: {document['_id']} with fields: {update_fields}")
+            count += 1
+    logger.info(f"Converted {count} documents to double")
+
+
+def canonicalize_extracted_keywords(collection):
+    keyword_mapping = {
+        'react.js': 'React',
+        'ReactJS': 'React',
+        'react': 'React',
+        'React.js': 'React',
+        'reactjs': 'React',
+        'React JS': 'React',
+        'REACT': 'React',
+        'React Query': 'React',
+        'React.JS': 'React',
+        'React Hooks': 'React',
+        'React Router': 'React',
+        'Reactjs': 'React',
+        'React-Query': 'React',
+        'React Js': 'React',
+
+        'golang': 'Go',
+        'Golang': 'Go',
+        'GoLang': 'Go',
+        'GO': 'Go',
+        
+        'Python 3': 'Python',
+        'python': 'Python',
+        'Python3': 'Python',
+        'Python scripting': 'Python',
+        'Python/Django': 'Python',
+        'PYTHON': 'Python',
+        'Spark Python': 'Python',
+
+        'MS SQL': 'Microsoft SQL Server',
+        'MS SQL Server': 'Microsoft SQL Server',
+        'MSSQL': 'Microsoft SQL Server',
+        'Microsoft SQL': 'Microsoft SQL Server',
+        'MS-SQL': 'Microsoft SQL Server',
+        'SQL Server': 'Microsoft SQL Server',
+        'Microsoft SQL Server 2016': 'Microsoft SQL Server',
+        'MSSQL Server': 'Microsoft SQL Server',
+        
+        'Django Rest Framework': 'Django',
+
+        
+        'Amazon Web Services (AWS)': 'AWS',
+        'AWS services': 'AWS',
+        'Amazon AWS': 'AWS',
+        'AWS cloud': 'AWS',
+        'aws': 'AWS',
+
+        'git': 'Git',
+        'GIT': 'Git',
+
+        'github': 'GitHub',
+        'Github': 'GitHub',
+        'GITHUB': 'GitHub',
+
+        'github actions': 'GitHub Actions',
+        'Github Actions': 'GitHub Actions',
+        'GitHub actions': 'GitHub Actions',
+        'Github actions': 'GitHub Actions',
+        'Github Action': 'GitHub Actions',
+
+        'Google Cloud Platform': 'GCP',
+        'Google Cloud': 'GCP',
+        'Google Cloud Platform (GCP)': 'GCP',
+        'Google GCP': 'GCP',
+        'Google cloud': 'GCP',
+
+        'Javascript': 'JavaScript',
+        'javascript': 'JavaScript',
+        'JavaScript (ES6+)': 'JavaScript',
+        'JavaScript ES6': 'JavaScript',
+        'JavaScript Frameworks': 'JavaScript',
+
+        'NodeJS': 'Node.js',
+        'Node': 'Node.js',
+        'Node.JS': 'Node.js',
+        'Node JS': 'Node.js',
+        'node.js': 'Node.js',
+        'NodeJs': 'Node.js',
+        'Nodejs': 'Node.js',
+        'node': 'Node.js',
+        'Node Js': 'Node.js',
+        'Node js': 'Node.js',
+
+        'Typescript': 'TypeScript',
+        'typescript': 'TypeScript',
+
+        'NoSQL databases': 'NoSQL',
+        'NoSQL Databases': 'NoSQL',
+        'NOSQL': 'NoSQL',
+        'noSQL': 'NoSQL',
+        'NoSQL Database': 'NoSQL',
+        'NoSQL database': 'NoSQL'
+
+    }
+    
+    # Query to find documents where extracted_keywords is a non-empty array containing specific keywords to be canonicalized
+    query = {
+        "extracted_keywords": {
+            "$type": "array",
+            "$elemMatch": {"$in": list(keyword_mapping.keys())}
+        }
+    }
+    
+    # Iterate over all documents that match the query
+    count = 0
+    for document in collection.find(query):
+        logger.info(f"Processing document ID: {document['_id']}")
+        logger.info(f"Before update: {document['extracted_keywords']}")
+        
+        updated_keywords = []
+        updated = False
+        
+        for keyword in document.get("extracted_keywords", []):
+            if keyword in keyword_mapping:
+                updated_keywords.append(keyword_mapping[keyword])
+                updated = True
+            else:
+                updated_keywords.append(keyword)
+        
+        # Update the document if any keyword was updated
+        if updated:
+            collection.update_one({"_id": document["_id"]}, {"$set": {"extracted_keywords": updated_keywords}})
+            logger.info(f"Updated document ID: {document['_id']} with new keywords: {updated_keywords}")
+            
+            # Read the document again to show the updated state
+            updated_document = collection.find_one({"_id": document["_id"]})
+            logger.info(f"After update: {updated_document['extracted_keywords']}")
+            count += 1
+        else:
+            logger.info("No keywords were updated for document ID: {document['_id']}.")
+    
+    logger.info(f"Canonicalized keywords in {count} documents")
